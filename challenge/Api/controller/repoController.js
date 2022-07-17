3d8c3654e27f7db9e84b9eabc9c8bf3ef09558fd
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-07-17 15:55:40 -0300

    refactor: change method and variable names

--- challenge/Api/controller/repoController.js
+++ challenge/Api/controller/repoController.js
@@ -1,18 +1,20 @@
 const repoService = require('../service/repoService');
 
-const getAll = async (_req, res) => {
-  const result = await repoService.getAll();
+const findOldest = async (_req, res) => {
+  const { message, code } = await repoService.findOldest('takenet');
+  if (code !== 200) return res.status(code).json(message);
+
   const obj = {
-    1: result[0],
-    2: result[1],
-    3: result[2],
-    4: result[3],
-    5: result[4],
+    1: message[0],
+    2: message[1],
+    3: message[2],
+    4: message[3],
+    5: message[4],
   };
 
-  return res.status(200).json(obj);
+  return res.status(code).json(obj);
 };
 
 module.exports = {
-  getAll,
+  findOldest,
 };
