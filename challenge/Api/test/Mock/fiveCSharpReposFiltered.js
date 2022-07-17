7cf4f1dcec889601a5025b316c35af398dc9abb4
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-07-17 15:26:07 -0300

    test: add mocks files

---
+++ challenge/Api/test/Mock/fiveCSharpReposFiltered.js
@@ -0,0 +1,30 @@
+module.exports = {
+  message: [
+    {
+      name: 'library.data',
+      description: 'Provides a simple abstraction for implementing the repository and unit of work patterns for data-enabled applications',
+      avatarUrl: 'https://avatars.githubusercontent.com/u/4369522?v=4'
+    },
+    {
+      name: 'library.logging',
+      description: 'Provides a simple logging interface for applications and some basic implementations of this interface',
+      avatarUrl: 'https://avatars.githubusercontent.com/u/4369522?v=4'
+    },
+    {
+      name: 'libphonenumber-csharp',
+      description: 'Forking original c# port',
+      avatarUrl: 'https://avatars.githubusercontent.com/u/4369522?v=4'
+    },
+    {
+      name: 'Takenet.Radar',
+      description: 'Radar de tecnologias takenet',
+      avatarUrl: 'https://avatars.githubusercontent.com/u/4369522?v=4'
+    },
+    {
+      name: 'Takenet.ScoreSystem',
+      description: 'Takenet score system',
+      avatarUrl: 'https://avatars.githubusercontent.com/u/4369522?v=4'
+    }
+  ],
+  code: 200
+}
